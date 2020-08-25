3330b815da793ac09bc97c330c2c5775979894e2
Tarmo Vannas <tarmo@soramitsu.co.jp>
2020-08-25 13:43:16 +0700

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,48 @@
-//
+(function ($) {
+  $.fn.extend({
+    rotaterator: function (options) {
+
+      var defaults = {
+        fadeSpeed: 500,
+        pauseSpeed: 2500,
+        child: null
+      };
+
+      var options = $.extend(defaults, options);
+
+      return this.each(function () {
+        var o = options;
+        var obj = $(this);
+        var items = $(obj.children(), obj);
+        items.each(function () {
+          $(this).hide();
+        })
+        if (!o.child) {
+          var next = $(obj).children(':first');
+        } else {
+          var next = o.child;
+        }
+        $(next).fadeIn(o.fadeSpeed, function () {
+          $(next).delay(o.pauseSpeed).fadeOut(o.fadeSpeed, function () {
+            var next = $(this).next();
+            if (next.length == 0) {
+              next = $(obj).children(':first');
+            }
+            $(obj).rotaterator({
+              child: next,
+              fadeSpeed: o.fadeSpeed,
+              pauseSpeed: o.pauseSpeed
+            });
+          })
+        });
+      });
+    }
+  });
+})(jQuery);
+
+$(document).ready(function () {
+  $('.rotate').rotaterator({
+    fadeSpeed: 500,
+    pauseSpeed: 2500
+  });
+});
