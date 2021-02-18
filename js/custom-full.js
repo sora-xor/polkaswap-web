7bc1e3e2c2ea2251b4d6b5c6a1232041e5c8672e
Tarmo Vannas <tarmo@tarmovannas.com>
2021-02-18 23:40:34 +0700

    Add files via upload

--- js/custom-full.js
+++ js/custom-full.js
@@ -45,4 +45,33 @@ $(document).ready(function () {
     fadeSpeed: 500,
     pauseSpeed: 2500
   });
+	
+	window.cookieconsent.initialise({
+		container: document.getElementById("cookieconsent"),
+		content: {
+			header: 'Cookies used on the website',
+			message: 'We use cookies to provide the best site experience.',
+			dismiss: 'Accept and don\'t show again',
+			allow: 'Allow cookies',
+			deny: 'Decline',
+			link: 'Learn more',
+			href: '/privacy',
+			close: '&#x274c;',
+			policy: 'Privacy Policy',
+			target: '_blank',
+		},
+		palette: {
+			 popup: {background: '#000000', text: '#fff', link: '#ff009a'},
+			 button: {background: '#fff', border: '##fff', text: '#000'},
+			 highlight: {background: '#ff009a', border: '#ff009a', text: '#fff'},
+		},
+		revokable: false,
+		onStatusChange: function(status) {
+			console.log(this.hasConsented() ?
+			'enable cookies' : 'disable cookies');
+		},
+		"theme": "edgeless",
+		"position": "bottom",
+	});
+	
 });
